04ac720d5675a3e62a90cde968f5719375373b0b
Organicstore151 <ziko.kkzz@gmail.com>
2025-04-08 20:36:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -173,8 +173,6 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-const twilio = require("twilio");
-
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 const sendTestNewsletter = async () => {
