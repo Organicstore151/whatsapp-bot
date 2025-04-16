a65a776c0927a10d931d9c98fa906899ac1258f0
Organicstore151 <ziko.kkzz@gmail.com>
2025-04-16 20:24:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -178,7 +178,6 @@ app.post("/webhook", async (req, res) => {
     messagingServiceSid: process.env.MESSAGING_SERVICE_SID,
     contentSid: 'HX639de1bcadce708027d192602ccb37e3',
   });
-}
       session.step = "waiting_for_command";
     } else {
       await client.messages.create({
